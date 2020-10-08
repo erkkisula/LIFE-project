95adba08fcf5a773155349668203be4e5bcd42b9
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2020-10-08 16:33:52 +0300

    Added main menu to game
    * Main menu UI
    
    * Minor fixes
    
    * TMP files
    
    * Main menu functionality

---
+++ Assets/menu_btns.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu_btns : MonoBehaviour
+{
+    // TODO: Add play game functionality
+
+    public void QuitGame() {
+        Debug.Log("Game quit event triggered");
+        Application.Quit();
+    }
+}
