1cfd78a9c90be5b7741c6b196e50863b025514fd
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2020-11-12 14:44:39 +0200

    Intro scene with temp assets (#3)

--- Assets/menu_btns.cs
+++ Assets/menu_btns.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menu_btns : MonoBehaviour
 {
-    // TODO: Add play game functionality
+  public void PlayGame()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
 
-    public void QuitGame() {
-        Debug.Log("Game quit event triggered");
-        Application.Quit();
-    }
+  public void QuitGame()
+  {
+    Debug.Log("Game quit event triggered");
+    Application.Quit();
+  }
 }
