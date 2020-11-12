6bcce9648a1b8dd9f44403015d30836b1482b3fd
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2020-11-12 14:43:24 +0200

    Intro scene with temp assets

---
+++ Assets/intro_script.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class intro_script : MonoBehaviour
+{
+    private int step = 1;
+    private int maxSteps = 3;
+    private string[] textStrings = {"STRING_1", "STRING_2", "STRING_3"};
+    public TextMeshProUGUI DialogText;
+
+    public void iterateStep() {
+        if(step < 3) {
+            step++;
+            DialogText.text = textStrings[step - 1];
+        } else {
+            DialogText.text = "NEXT_SCENE";
+        }
+
+    }
+    
+    void Start()
+    {
+        DialogText.text = textStrings[0];
+        
+    }
+
+    void Update () {
+
+    }
+}
