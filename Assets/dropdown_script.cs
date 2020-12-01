3aa93d616042cfc0eeff0bcd95e84c1076e29709
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2020-12-01 21:52:47 +0200

    Added dropdowns and script

---
+++ Assets/dropdown_script.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class dropdown_script : MonoBehaviour
+{
+    public int selectedValue = 0;
+
+    public void handleInput(int val) {
+        selectedValue = val;
+        print(val);
+    }
+}
