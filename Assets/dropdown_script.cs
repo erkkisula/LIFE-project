942ec2c1ece0e714b1d1c310c0044f9811d0d96a
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2020-12-03 00:00:33 +0200

    Feature/interaction menu (#10)
    * Added new assets
    
    * Added item script
    
    * Added dropdowns and script
    
    * Interaction menu functionality

---
+++ Assets/dropdown_script.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class dropdown_script : MonoBehaviour
+{
+    public int previousValue = 0;
+    public int selectedValue = 0;
+
+    public bool isUiEvent = true;
+
+    public TMP_Dropdown thisDropdown;
+
+    private GameObject controller;
+
+    void Start()
+    {
+        controller = GameObject.Find("Menu_Panel");
+    }
+
+    public void handleInput(int val)
+    {
+        if (isUiEvent)
+        {
+            controller.GetComponent<int_menu_script>().swapObjects(val, selectedValue);
+            previousValue = selectedValue;
+            selectedValue = val;
+        }
+        else
+        {
+            isUiEvent = true;
+        }
+
+    }
+
+    public void setDropdownValue(int newVal)
+    {
+        isUiEvent = false;
+        previousValue = selectedValue;
+        selectedValue = newVal;
+        thisDropdown.value = selectedValue;
+    }
+}
---
+++ Assets/item_script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item_script : MonoBehaviour
+{
+    public Vector2 position = new Vector2(0, 0);
+    public string itemName = null;
+    public int itemId = 0;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    public void setPosition(Vector2 newPos)
+    {
+        position = newPos;
+        transform.position = position;
+    }
+}
